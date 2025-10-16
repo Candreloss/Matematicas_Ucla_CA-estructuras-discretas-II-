6ae8e8b2344178a8c26f2d18b41eade4d27a437e
RicardoPerez13 <1001.30995733.ucla@gmail.com>
2025-10-16 16:26:49 -0400

    Constantes, colores y fuentes

---
+++ constantes.py
@@ -0,0 +1,13 @@
+#Colores para la interfaz gráfica
+AZULP = ("#2f2978")
+AZULS = ("#38A4DD")
+LILA = ("#C4C5E5")
+BLANCO = ("#FFFFFF")
+NEGRO = ("#000000")
+GRIS = ("#666666")
+
+#Tipografias
+verdanaCursivaN = ("Recursos/verdanaz.ttf", 20, )
+verdanaCursiva = ("Recursos/verdanai.ttf", )
+verdanaNegra = ("Recursos/verdanab.ttf", )
+verdanaN = ("Recursos/verdanab.ttf", )
--- interface.py
+++ interface.py
@@ -1,7 +1,7 @@
 #Importación de las librerías
 import tkinter as tk
 from tkinter import ttk
-
+import constantes
 #Incialización del programa, configuración y personalización de las características de la ventana principal
 app = tk.Tk()
 app.title("Mátemáticas UCLA C.A")
@@ -12,23 +12,23 @@ style.theme_use("clam")
 
 #Implementación de los estilos de la interfaz
 
-style.configure("label_title.TLabel", background="#666666", foreground="#E7E7E7", relief=tk.FLAT, font=("Forte", 20, ), anchor="center")
-style.configure("bfs.TButton", background="#255255", foreground="#ffffff", relief=tk.FLAT, font=("Tahoma", 8, ))
-style.map("bfs.TButton", background=[("active", "#8585f7")], foreground=[("active", "#ffffff")])
-style.configure("dfs.TButton", background="#17738F", foreground="#ffffff", relief=tk.FLAT, font=("Tahoma", 8, ))
-style.map("dfs.TButton", background=[("active", "#8585f7")], foreground=[("active", "#ffffff")])
-style.configure("kruskal.TButton", background="#17738F", foreground="#ffffff", relief=tk.FLAT, font=("Tahoma", 8, ))
-style.map("kruskal.TButton", background=[("active", "#8585f7")], foreground=[("active", "#ffffff")])
-style.configure("prim.TButton", background="#17738F", foreground="#ffffff", relief=tk.FLAT, font=("Tahoma", 8, ))
-style.map("prim.TButton", background=[("active", "#8585f7")], foreground=[("active", "#ffffff")])
-style.configure("integrantes.TButton", background="#050505", foreground="#ffffff", relief=tk.FLAT, font=("Tahoma", 8, ))
-style.map("integrantes.TButton", background=[("active", "#17738F")], foreground=[("active", "#ffffff")])
+style.configure("label_title.TLabel", background=constantes.AZULP, foreground="#FFFFFF", relief=tk.FLAT, font=("Verdana", 20, "bold", "italic"), anchor="center")
+style.configure("bfs.TButton", background="#2f2978", foreground="#ffffff", relief=tk.FLAT, font=("Verdana", 12, "bold" ))
+style.map("bfs.TButton", background=[("active", "#38A4DD")], foreground=[("active", "#ffffff")])
+style.configure("dfs.TButton", background="#2f2978", foreground="#ffffff", relief=tk.FLAT, font=("Verdana", 8, "bold" ))
+style.map("dfs.TButton", background=[("active", "#38A4DD")], foreground=[("active", "#ffffff")])
+style.configure("kruskal.TButton", background="#2f2978", foreground="#ffffff", relief=tk.FLAT, font=("Verdana", 8, "bold" ))
+style.map("kruskal.TButton", background=[("active", "#38A4DD")], foreground=[("active", "#ffffff")])
+style.configure("prim.TButton", background="#2f2978", foreground="#ffffff", relief=tk.FLAT, font=("Verdana", 8, "bold" ))
+style.map("prim.TButton", background=[("active", "#38A4DD")], foreground=[("active", "#2f2978")])
+style.configure("integrantes.TButton", background="#38A4DD", foreground="#2f2978", relief=tk.FLAT, font=("Verdana", 8, "bold" ))
+style.map("integrantes.TButton", background=[("active", "#C4C5E5")], foreground=[("active", "#2f2978")])
 
 
 #Título
 
 frame_title = tk.Frame(master=app)
-frame_title.config(bg="#17738F")
+frame_title.config(bg="#2f2978")
 frame_title.place(x=0, y=0, width=800, height=50)
 
 label_title = ttk.Label(master=frame_title, text="Matemáticas UCLA C.A", style="label_title.TLabel")
@@ -38,7 +38,7 @@ label_title.place(x=0, y=0, width=800, height=50)
 #Menú principal
 
 frame_menu = tk.Frame(master=app)
-frame_menu.config(bg="#f7f7f7")
+frame_menu.config(bg="#C4C5E5")
 frame_menu.place(x=20, y=70, width=270, height=410)
 
 bfs = ttk.Button(master=frame_menu, text="Recorrido de anchura (BFS)", style="bfs.TButton")
@@ -64,10 +64,11 @@ frame_grafo.place(x=311, y=96, width=460, height=344)
 # Footer
 
 frame_footer = tk.Frame(master=app)
-frame_footer.config(bg="#17738F")
+frame_footer.config(bg="#2f2978")
 frame_footer.place(x=0, y=500, width=800, height=50)
 
 integrantes = ttk.Button(master=frame_footer, text="👤", style="integrantes.TButton")
+
 integrantes.place(x=750, y=5, width=40, height=40)
 
 
