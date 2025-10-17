050a387858b213baeaf5762689c92d3d9998f024
Karen <karenmrv10@gmail.com>
2025-10-16 21:52:31 -0400

    Update Modelo_Grafo.py

--- Modelo_Grafo.py
+++ Modelo_Grafo.py
@@ -14,7 +14,7 @@ class Grafo:
             self.G= nx.Graph()
         self.vertice = ['A', 'B', 'C', 'E', 'D', 'F', 'G', 'H',  'I', 'K', 'L', 'M', 'N', 'P']
         self.peso = [("A", "B", 8), ("A", "D", 5), ("A", "E", 4), ("B", "C", 3), ("B", "F", 4), ("B", "E", 4), ("C", "F", 6), ("C", "G", 7), ("D", "E", 1), ("D", "I", 2), ("D", "H", 3), ("E", "F", 3), ("E", "I", 2), ("F", "G", 1), ("F", "K", 14), ("F", "I", 3), ("G", "K", 2), ("G", "L", 3), ("H", "I", 11), ("H", "M", 6), ("I", "K", 6), ("I", "P", 15), ("I", "N", 2), ("I", "M", 5), ("K", "L", 8), ("K", "P", 3), ("L", "P", 6), ("M", "N", 1), ("N", "P", 13)]
-        self.pos= {'A': (0, 0), 'B': (0, 0), 'C': (0, 0), 'E': (0, 0), 'D': (0, 0), 'F': (0, 0), 'G': (0, 0), 'H': (0, 0),  'I': (0, 0), 'K': (0, 0), 'L': (0, 0), 'M': (0, 0), 'N': (0, 0), 'P': (0, 0)}
+        self.pos= {'A': (-100, 150),'B': (10, 225), 'C': (80, 305), 'D': (-15, 50), 'E': (85, 150), 'F': (160, 225), 'G': (230, 305), 'H': (141.26, 10), 'I': (300, 100), 'K': (310, 225), 'L': (380, 305), 'M': (280, 20), 'N': (450, 80), 'P': (460, 225) }
         self.G.add_nodes_from(self.vertice)
         self.G.add_weighted_edges_from(self.peso)
         # Los pesos estan en un diccionario con tuplas (u,v) como llaves 
@@ -31,23 +31,23 @@ class Grafo:
             ax = fig.add_subplot(111) # Añadimos un sub-gráfico
 
             # Determinación de las posiciones
-            pos = nx.spring_layout(grafo.G, seed=100, k=0.5, iterations=100)
                 
             # Dibujar el grafo
             nx.draw(
                 grafo.G, 
-                pos=pos, 
+                pos=grafo.pos, 
                 ax=ax, # ¡Importante! Dibujar en el sub-gráfico 'ax'
                 with_labels=True, 
                 node_color="#8585f7", # Color azul que se acerca a tu esquema
-                node_size=800, 
+                node_size=300, 
+                font_size=8,
                 font_weight="bold",
                 font_color="black"
             )
                 
             # Mostrar los pesos de las aristas
             labels = nx.get_edge_attributes(grafo.G, 'weight')
-            nx.draw_networkx_edge_labels(grafo.G, pos, edge_labels=labels, ax=ax)
+            nx.draw_networkx_edge_labels(grafo.G, grafo.pos, edge_labels=labels, ax=ax)
                 
             ax.set_title("Grafo de la Red") # Establecer el título
             ax.axis('off') # Ocultar los ejes de Matplotlib
