6e5ba939d07447dc570c08ebcaa34a21c1d9ec09
Karen <karenmrv10@gmail.com>
2025-10-16 20:13:57 -0400

    Add files via upload

---
+++ constantes.py
@@ -0,0 +1,16 @@
+from tkinter import font
+#Colores para la interfaz gráfica
+AZULP = ("#2f2978")
+AZULS = ("#38A4DD")
+LILA = ("#C4C5E5")
+BLANCO = ("#FFFFFF")
+NEGRO = ("#000000")
+GRIS = ("#666666")
+
+#Tipografias
+def get_fonts(app):
+    verdanaCursivaN = font.Font(root=app, family="Verdana", size=20, slant="italic", weight="bold")
+    verdanaCursiva  = font.Font(root=app, family="Verdana", size=16, slant="italic")
+    verdanaNegra    = font.Font(root=app, family="Verdana", size=10, weight="bold")
+    verdanaN        = font.Font(root=app, family="Verdana", size=12)
+    return verdanaCursivaN, verdanaCursiva, verdanaNegra, verdanaN
